9937f861e52b041bbacdecf60060ad1e55ddccfb
Preet Nagda <preetnagda34@gmail.com>
2023-05-16 12:33:11 +1000

    Add cors to all

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI, Depends
 from dotenv import load_dotenv
 from .utils.config import Config
 from .db import db
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 config = Config()
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
